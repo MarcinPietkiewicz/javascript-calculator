2f94e1ccbdfe79d78fc9963e2a23eaa8d54549f6
Marcin <marcin.p.dev@gmail.com>
2022-10-19 12:28:06 +0200

    Separated calculator into component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import './App.css';
+import Calculator from './Calculator.js';
 
 function App() {
   return (
     <div className="App">
-        <div className="Calculator">
-          <div id="title">ReactJS Calculator</div>
-          <div id="display-screen">
-            <div id="display-result">34+56=54</div> {/* optional */}
-            <div id="display">54</div>
-          </div>
-          <div id="clear">AC</div>
-          <div id="divide">/</div>
-          <div id="multiply">x</div>
-          <div id="seven">7</div>
-          <div id="eight">8</div>
-          <div id="nine">9</div>
-          <div id="subtract">-</div>
-          <div id="four">4</div>
-          <div id="five">5</div>
-          <div id="six">6</div>
-          <div id="add">+</div>
-          <div id="one">1</div>
-          <div id="two">2</div>
-          <div id="three">3</div>
-          <div id="equals">=</div>
-          <div id="zero">0</div>
-          <div id="decimal">.</div>
-        </div>
+       <Calculator />
     </div>
   );
 }

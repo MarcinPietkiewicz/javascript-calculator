7b7994501a3810c4c475f79765a94ee4b1e16bd3
Marcin <marcin.p.dev@gmail.com>
2022-10-19 12:21:40 +0200

    Initial CSS styling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      Hello world
-      </header>
+        <div className="Calculator">
+          <div id="title">ReactJS Calculator</div>
+          <div id="display-screen">
+            <div id="display-result">34+56=54</div> {/* optional */}
+            <div id="display">54</div>
+          </div>
+          <div id="clear">AC</div>
+          <div id="divide">/</div>
+          <div id="multiply">x</div>
+          <div id="seven">7</div>
+          <div id="eight">8</div>
+          <div id="nine">9</div>
+          <div id="subtract">-</div>
+          <div id="four">4</div>
+          <div id="five">5</div>
+          <div id="six">6</div>
+          <div id="add">+</div>
+          <div id="one">1</div>
+          <div id="two">2</div>
+          <div id="three">3</div>
+          <div id="equals">=</div>
+          <div id="zero">0</div>
+          <div id="decimal">.</div>
+        </div>
     </div>
   );
 }

b92633ea781e61b4fc1ae39ab8672455dc53b806
Marcin <marcin.p.dev@gmail.com>
2022-10-19 13:11:28 +0200

    Added AC button functionality

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,23 +4,25 @@ import { evaluate } from 'mathjs';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { expression: "", result: "544" };
+    this.state = { expression: "500+44=544", display: "544" };
     this.clearResult = this.clearResult.bind(this);
   }
 
   clearResult() {
-    this.setState({ result: "0" });
+    this.setState({ expression: '', display: "0" });
   }
+
+
 
   render() {
     return (
       <div className="Calculator">
         <div id="title">ReactJS Calculator</div>
         <div id="display-screen">
-          <div id="display-result">9/3+21</div>
-          <div id="display">{evaluate('9/3+21')}</div>
+          <div id="display-result">{this.state.expression}</div>
+          <div id="display">{this.state.display}</div>
         </div>
-        <div id="clear">AC</div>
+        <div id="clear" onClick={this.clearResult}>AC</div>
         <div id="divide">/</div>
         <div id="multiply">x</div>
         <div id="seven">7</div>
